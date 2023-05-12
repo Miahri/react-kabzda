87c487335cb02d6d54949cc4bdda335818c35c0e
Miahri <mahrihommadova@gmail.com>
2023-05-12 17:50:16 +0300

    example of using setTimeout with useEffect

---
+++ src/stories/SetTimeout_Interval.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: 'useEffect'
+}
+
+export function SetTimeoutExample () {
+    const [count, setCount] = useState(0);
+    const [fake, setFake] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = count.toString();
+        }, 1000)
+    }, [])
+
+    return <>
+        <button onClick={() => setCount(() => count + 1)}>Count</button>
+        <button onClick={() => setFake(() => fake + 1)}>Fake</button>
+        {count + ' ' + fake}
+    </>
+}
