4e7aec08c6c95f24068c809cb52c96533c6a8f47
Miahri <mahrihommadova@gmail.com>
2023-05-25 16:24:16 +0300

    added new component Clock for example of using useEffect with setInterval and clearing interval and added stories for Clock

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 type AccordionPropsType = {
     title: string
---
+++ src/components/Clock/Clock.stories.tsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from "react";
+import {Clock} from "./Clock";
+
+export default {
+    title: 'Clock',
+    component: Clock
+}
+
+export function ClockExample () {
+    return <Clock/>
+}
